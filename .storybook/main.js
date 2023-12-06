50ea62adf56adf58b28e5738c0ff5206ebb85389
Boris Dryabin <b.dryabin@rt-solar.ru>
2023-12-06 21:30:36 +0300

    починил проект

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+
+const config = {
+  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|ts)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
+  ],
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
--- src/components/toggler/toggler.vue
+++ src/components/toggler/toggler.vue
@@ -1,30 +1,30 @@
 <template>
-<button :class="['button', { active: isOpened }]" @click="toggle">
-<span class="text">{{ isOpened ? "Hide" : "Show" }} issues</span>
-<span class="icon">
-<icon name="triangle" />
-</span>
-</button>
+  <button :class="['button', { active: isOpened }]" @click="toggle">
+    <span class="text">{{ isOpened ? 'Hide' : 'Show' }} issues</span>
+    <span class="icon">
+      <icon name="triangle" />
+    </span>
+  </button>
 </template>
 
 <script>
-import { icons } from '../../icons'
+import { icon } from '../../icons';
 
 export default {
-    component: {
-        icons
-    },
-    data () {
-        return {
-            isOpened: false
-        }
-    },
-    methods: {
-        toggle () {
-            this.isOpened = !this.isOpened;
-            this.$emit('onToggle', this.isOpened)
-        }
+  component: {
+    icon
+  },
+  data () {
+    return {
+      isOpened: false
+    };
+  },
+  methods: {
+    toggle () {
+      this.isOpened = !this.isOpened;
+      this.$emit('onToggle', this.isOpened);
     }
+  }
 };
 </script>
 
